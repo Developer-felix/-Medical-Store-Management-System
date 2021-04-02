e18b0a41fa876f00ee4a0840dcea264ab8be15f8
Developer-felix <onjombafelix1@gmail.com>
2021-04-02 10:02:18 -0700

    Database Design

--- MedicalStoreApp/models.py
+++ MedicalStoreApp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    lisence_no = models.CharField(max_length=255)
+    adress = models.CharField(max_length=255)
+    contact_no = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    added_on = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+class Medicine(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    medical_type = models.CharField(max_length=255)
+    buy_price = models.CharField(max_length=255)
+    sell_price = models.CharField(max_length=255)
+    c_gst = models.CharField(max_length=255)
+    s_gst = models.CharField(max_length=255)
+    batch_no = models.CharField(max_length=255)
+    shelf_no = models.CharField(max_length=255)
+    expiry_date = models.DateField(max_length=255)
+    mfg_date = models.DateField(max_length=255)
+    company_id = models.ForeignKey(Company, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    in_stock_total = models.IntegerField()
+    qty_in_strip = models.IntegerField()
+    added_on = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class MedicalDetails(models.Model):
+    id = models.AutoField(primary_key=True)
+    medicine_id = models.ForeignKey(Medicine, on_delete=models.CASCADE)
+    salt_name = models.CharField(max_length=255)
+    salt_qty = models.CharField(max_length=255)
+    salt_qty_type = models.CharField(max_length=255)
+    added_on = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=255)
+    objects = models.Manager()
