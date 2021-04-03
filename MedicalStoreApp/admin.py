929dc42622ae84deb17af774f82a1215530da16c
Developer-felix <onjombafelix1@gmail.com>
2021-04-03 12:24:19 -0700

    Model Registration

--- MedicalStoreApp/admin.py
+++ MedicalStoreApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from MedicalStoreApp.models import Company,CompanyBank,Medicine,MedicalDetails,Employee,EmployeeSalary,EmployeeBank,Customer,CustomerRequest,CompanyAccount,Bill,BillDetails
 
-# Register your models here.
+admin.site.register(Company)
+admin.site.register(CompanyBank)
+admin.site.register(CompanyAccount)
+admin.site.register(Medicine)
+admin.site.register(MedicalDetails)
+admin.site.register(Employee)
+admin.site.register(Customer)
+admin.site.register(CustomerRequest)
+admin.site.register(EmployeeSalary)
+admin.site.register(EmployeeBank)
+admin.site.register(Bill)
+admin.site.register(BillDetails)
