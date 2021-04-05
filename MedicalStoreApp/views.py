79cac934629ab9693e0b4b2e0348342b67ab1c87
Developer-felix <onjombafelix1@gmail.com>
2021-04-05 11:54:14 -0700

    Changing staffs

--- MedicalStoreApp/views.py
+++ MedicalStoreApp/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from MedicalStoreApp.serializers import CompanySerializer
 from MedicalStoreApp.models import Company
+from rest_framework.response import Response
 
 class CompanyViewSet(viewsets.ModelViewSet):
     def list(self,request):
         Company = Company.objects.all()
-    queryset = CompanySerializer(Company,many=True,context={"request":request})
+        serializer = CompanySerializer(Company,many=True,context={"request":request})
+        response_dict = {"error":False,"Message":"All company list Data","data":serializer.data}
+        return Response(response_dict)
